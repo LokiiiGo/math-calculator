c395e797428478bb6cb6bdc194ea10fc0b9b92ea
LokiiiGo <lucascalle880@gmail.com>
2026-08-19 17:14:05 -0300

    Mudando a linguagem do documento html para inglês

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="en">
       <body
         className={`${arimo.variable} antialiased`}
       >
